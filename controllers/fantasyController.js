aa1040d31d7da6b11adde89adb90213dfd435bda
Keith Powell <keithpowell@Keiths-MacBook-Air.local>
2021-04-16 17:38:58 -0700

    Index route is functional

--- controllers/fantasyController.js
+++ controllers/fantasyController.js
@@ -1,7 +1,7 @@
 //TODO - Use all 7 RESTful routes
 /* 
   Restful Routes
-    Index - GET - /fantasyMovies -> Presentational
+    
     New - GET - /fantasyMovies/new -> Presentational Form
     Create - POST - /fantasyMovies -> Functional
     Show - GET - /fantasyMovies/id -> Presentational
@@ -15,5 +15,10 @@ const router = express.Router();
 const db = require("../models")
 
 // Index - GET - /fantasyMovies -> Presentational
+router.get("/", function (req, res) {
+    console.log("All your movies in one spot");
+})
+
+
 
 module.exports = router;
